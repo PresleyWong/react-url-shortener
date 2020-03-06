012598cb10978609b39d08b513485fcca5a04fe8
Presley <presley@pop-os.localdomain>
2020-03-07 01:39:09 +0800

    Updates

---
+++ precache-manifest.fedf117dda253ead769b5e0d89159363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25574a5b1f1cd2ba373bc8cfe258914f",
+    "url": "/react-url-shortener/index.html"
+  },
+  {
+    "revision": "e6fc591febd11b1d9f00",
+    "url": "/react-url-shortener/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6a01fdd51bd07fa6b358",
+    "url": "/react-url-shortener/static/css/main.24517def.chunk.css"
+  },
+  {
+    "revision": "e6fc591febd11b1d9f00",
+    "url": "/react-url-shortener/static/js/2.57c65a3c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-url-shortener/static/js/2.57c65a3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a01fdd51bd07fa6b358",
+    "url": "/react-url-shortener/static/js/main.f014099b.chunk.js"
+  },
+  {
+    "revision": "0e1d2bc94829f3131afb",
+    "url": "/react-url-shortener/static/js/runtime-main.8fb61f4b.js"
+  }
+]);
